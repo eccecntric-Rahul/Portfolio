f8b9d52a7f3f2a1e2a2d92c035eabdeac7f22975
Rahul Kumar <rahul_kumar@applicatetechnology.com>
2023-02-07 11:07:33 +0530

    corrected education schema

--- model/EducationSchema.js
+++ model/EducationSchema.js
@@ -19,7 +19,46 @@ const educationSchema= new Schema({
         trim: true,
         required: "marks are required",
     },
-    
+    location: {
+        type: String,
+        trim: true,
+        required: "location are required",
+    },
+    duration:{
+        type: String,
+        trim: true,
+        required: "duration are required",
+    },
+    doneFrom:{
+        type:String,
+    },
+    matched:{
+        type:String,
+        trim:true,
+        default:'98'
+    },
+    liked:{
+     type:Boolean,
+     default:false,
+    },
+    orderId:{
+        type:Number,
+        required:true,
+    },
+    bgColor:{
+        type:String,
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    training:{
+        type:Boolean,
+        default:true,
+    },
+    goToLink:{
+        type:String,
+    }
 });
 
 
