96eade0846628cf6960a85d22f13dff3b568a908
Rahul Kumar <rahul_kumar@applicatetechnology.com>
2023-02-10 23:33:03 +0530

    added nodemailer

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -7,6 +7,7 @@ import ProjectModel from "../model/ProjectSchema";
 import ReviewSchema from "../model/ReviewSchema";
 import SkillModel from "../model/SkillModel";
 import TrainingModel from "../model/TrainingSchema";
+import nodemailer from 'nodemailer';
 
 const postEducation= async (req,res)=>{
     try{
@@ -80,6 +81,34 @@ export const postProfile= async (req,res)=>{
 export const postReview= async (req,res)=>{
     try{
         const response = await ReviewSchema.create({...req.fields});
+        const testAccount = await nodemailer.createTestAccount();
+        let transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+              user: testAccount.user, // generated ethereal user
+              pass: testAccount.pass, // generated ethereal password
+            },
+          });
+          const text = `Name - ${req.fields.guestName}
+          Review - ${req.fields.comment}
+          Liked - ${req.fields.like==true?"Yes":"No"}`
+          // send mail with defined transport object
+          let info = await transporter.sendMail({
+            from: '"TechRahul" <techrahul.com>', // sender address
+            to: "rk2655415@gmail.com ,mokshitakhurana1998@gmail.com", // list of receivers
+            subject: "New Review Posted on TechRahul!", // Subject line
+            text: text, // plain text body
+            html: "<b></b>", // html body
+          });
+        
+          console.log("Message sent: %s", info.messageId);
+          // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        
+          // Preview only available when sending through an Ethereal account
+          console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+          // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
         return res.status(200).send(response);
     }catch(err){
         console.log(err)
